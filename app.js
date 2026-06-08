2ed65064ae50f74550851f1794d40c45b0624412
er1k-rapid <erikesmeraldo@hotmail.com>
2026-06-07 22:15:46 -0300

    feat: resolve a missao 5 - configuracao do babel e componente counter

---
+++ app.js
@@ -0,0 +1,22 @@
+/** @jsx Didact.createElement */
+
+function Counter() {
+  const [count, setCount] = Didact.useState(1);
+
+  return (
+    <div style="font-family: sans-serif; padding: 40px;">
+      <h1>Contagem: {count}</h1>
+      
+      <button onClick={() => setCount(c => c + 1)}>
+        + Incrementar
+      </button>
+      
+      <button onClick={() => setCount(c => c - 1)}>
+        - Decrementar
+      </button>
+    </div>
+  );
+}
+
+const container = document.getElementById("root");
+Didact.render(<Counter />, container);
